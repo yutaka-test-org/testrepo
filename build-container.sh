93164f2c1ac890f15e013e8509f95b450004ab86
yutaka-m <alumican2000@gmail.com>
2019-08-07 14:26:40 +0900

    Update build-container.sh

--- build-container.sh
+++ build-container.sh
@@ -2,9 +2,8 @@
 
 eval $(aws ecr get-login --region ap-northeast-1 | sed -e 's/-e none//')
 #docker build --no-cache=true -t t-$1:$3$build_args $1
-docker build --no-cache=true -t yutaka-testrepo:latest yutaka-testrepo
+docker build -t yutaka-testrepo:latest .
 #docker tag t-$1:$3 .dkr.ecr.ap-northeast-1.amazonaws.com/t-$1:$3
 docker tag yutaka-testrepo:latest 566423514641.dkr.ecr.ap-northeast-1.amazonaws.com/yutaka-testrepo:latest
 #docker push .dkr.ecr.ap-northeast-1.amazonaws.com/t-$1:$3
 docker push 566423514641.dkr.ecr.ap-northeast-1.amazonaws.com/yutaka-testrepo:latest
-
